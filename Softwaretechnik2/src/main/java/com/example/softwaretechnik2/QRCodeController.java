b7671e856e286d038cf12d804f2bbc8b8641f3c3
348364 <igor.greszta@stud.th-luebeck.de>
2023-04-30 19:46:46 +0200

    RequestParam in QRCodeController

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/QRCodeController.java
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/QRCodeController.java
@@ -1,5 +1,6 @@
 package com.example.softwaretechnik2;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
