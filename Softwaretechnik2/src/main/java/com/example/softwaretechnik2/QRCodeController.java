da0aab9a6c5dde666114ba5cc8becdb146b451dc
348364 <igor.greszta@stud.th-luebeck.de>
2023-05-01 19:06:11 +0200

    Erste fertigstellung des QRCode - Generators Löschen der redundanten Klasse QRCodeGenerator.java Hinzufügen eines csrf - Parameter zur index.html

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/QRCodeController.java
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/QRCodeController.java
@@ -1,16 +1,14 @@
 package com.example.softwaretechnik2;
 
-import org.springframework.data.repository.query.Param;
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.io.ByteArrayOutputStream;
-
-import net.glxn.qrgen.core.image.ImageType;
-import net.glxn.qrgen.javase.QRCode;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.ByteArrayOutputStream;
 
 @Controller
 public class QRCodeController {
@@ -22,7 +20,8 @@ public class QRCodeController {
 
     @PostMapping("/QRCode")
     public String getQRCode(@RequestParam String link, Model model) {
-        ByteArrayOutputStream outputStream = QRCode.from(link).to(ImageType.PNG).stream();
+
+        ByteArrayOutputStream outputStream = QRCode.from(link).withSize(400, 400).to(ImageType.PNG).stream();
         String qrCodeBase64 = java.util.Base64.getEncoder().encodeToString(outputStream.toByteArray());
         model.addAttribute("qrCode", qrCodeBase64);
         return "qrCode";
