cd33dfaa9df87e6c29c0eb151e0d07f07466a3ba
kawtharalshaaer <Kawthar.alshaaer@stud.th-luebeck.de>
2023-05-23 15:53:19 +0200

    sql file to insert some products to DB

---
+++ Softwaretechnik2/src/main/resources/database/products.sql
@@ -0,0 +1,5 @@
+insert into product values (1, "vegetables", 1, "Potato");
+insert into product values (2, "vegetables", 0, "tomato");
+insert into product values (3, "fruits", 1, "apple");
+insert into product values (4, "vegetables", 1, "banana");
+insert into product values (5, "vegetables", 0, "dragon fruit");
