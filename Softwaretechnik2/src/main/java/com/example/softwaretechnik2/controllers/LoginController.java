395360bfafbbf8d99aaeda3229be53418e788522
Luis <luis.bruhn@stud.th-luebeck.de>
2023-06-02 18:31:28 +0200

    #45 removed unnessecary comment

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/controllers/LoginController.java
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/controllers/LoginController.java
@@ -16,9 +16,6 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login(Model model) {
-        //Optional<Product> product = repo.findById(1L);
-        //model.addAttribute("salamibrötchen",product.get().getProductName());
-
         return "login";
     }
 }
