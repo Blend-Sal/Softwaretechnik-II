567ef669639641024563bd11e84b6c3c6ebd11d2
kawtharalshaaer <Kawthar.alshaaer@stud.th-luebeck.de>
2023-06-06 16:44:19 +0200

    #17 customized user details service to provide core user information

---
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/services/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.example.softwaretechnik2.services;
+
+
+import com.example.softwaretechnik2.model.Role;
+import com.example.softwaretechnik2.model.User;
+import com.example.softwaretechnik2.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                    user.getPassword(),
+                    mapRolesToAuthorities(user.getRoles()));
+        }else{
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+    }
+
+    private Collection < ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
+
