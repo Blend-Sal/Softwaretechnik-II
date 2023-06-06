18d3b64a09c5374e2ebbd206d930507b45e82a30
kawtharalshaaer <Kawthar.alshaaer@stud.th-luebeck.de>
2023-06-06 16:42:06 +0200

    #17 updated registration post request

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/controllers/AuthController.java
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/controllers/AuthController.java
@@ -1,8 +1,6 @@
 package com.example.softwaretechnik2.controllers;
 
 import com.example.softwaretechnik2.model.User;
-import com.example.softwaretechnik2.repositories.ProductRepository;
-import com.example.softwaretechnik2.services.ProductService;
 import com.example.softwaretechnik2.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +10,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.hibernate.bytecode.enhance.spi.interceptor.BytecodeInterceptorLogging.LOGGER;
+
 @AllArgsConstructor
 @Controller
 public class AuthController {
@@ -34,17 +38,28 @@ public class AuthController {
 
     @PostMapping("/register/save")
     public String registration(@ModelAttribute("user") User user,
-                               BindingResult result,
+                               BindingResult result, HttpServletRequest request,
                                Model model) {
-        User existing = userService.findByEmail(user.getEmail());
+        String email = user.getEmail();
+        String password = user.getPassword();
+        String confirmedPassword = user.getConfirmedPassword();
+        User existing = userService.findByEmail(email);
         if (existing != null) {
             result.rejectValue("email", null, "bereits registriert!");
+        }
+        if (!password.equals(confirmedPassword)) {
+            result.rejectValue("password", null, "Passwörter stimmen nicht überein");
         }
         if (result.hasErrors()) {
             model.addAttribute("user", user);
             return "register";
         }
         userService.saveUser(user);
-        return "redirect:/login";
+        try {
+            request.login(email, password);
+        } catch (ServletException e) {
+            LOGGER.error("Error while login ", e);
+        }
+        return "redirect:/start";
     }
 }
