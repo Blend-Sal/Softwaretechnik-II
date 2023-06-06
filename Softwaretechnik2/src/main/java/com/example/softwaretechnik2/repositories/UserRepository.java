eecb364230095c60a1d6390e506f8b54a31790ec
AimanAlalie <aiman.al-alie@stud.th-luebeck.de>
2023-06-06 16:50:36 +0200

    #17 deleted unneeded imports

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/repositories/UserRepository.java
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/repositories/UserRepository.java
@@ -2,10 +2,7 @@ package com.example.softwaretechnik2.repositories;
 
 import com.example.softwaretechnik2.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
