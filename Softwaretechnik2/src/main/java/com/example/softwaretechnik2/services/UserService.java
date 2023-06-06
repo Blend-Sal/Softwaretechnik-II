eb59560b542fc97b52d740efd38097024df74728
AimanAlalie <aiman.al-alie@stud.th-luebeck.de>
2023-06-06 16:50:09 +0200

    #17 added functions to check user role

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/services/UserService.java
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.softwaretechnik2.services;
 
+import com.example.softwaretechnik2.model.Role;
 import com.example.softwaretechnik2.model.User;
 import com.example.softwaretechnik2.repositories.RoleRepository;
 import com.example.softwaretechnik2.repositories.UserRepository;
@@ -7,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 
@@ -19,11 +19,17 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role role = checkRole(user);
+        user.setRoles(List.of(role));
         userRepository.save(user);
     }
+
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
@@ -34,4 +40,13 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    private Role checkRole(User user) {
+        Role role = new Role();
+        if(user.getEmail().contains("@asta-shop")){
+            role.setName("ROLE_EMPLOYEE");
+        } else {
+            role.setName("ROLE_CLIENT");
+        }
+        return roleRepository.save(role);
+    }
 }
