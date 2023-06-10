c589705deb5cb2c6c1efe7913a7b9c9a0b1b51da
Blend <122615951+Blend-Sal@users.noreply.github.com>
2023-06-10 19:42:44 +0200

    Delete Softwaretechnik-II directory

--- Softwaretechnik-II/Softwaretechnik2/src/main/java/com/example/softwaretechnik2/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.softwaretechnik2.repositories;
-
-import com.example.softwaretechnik2.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
