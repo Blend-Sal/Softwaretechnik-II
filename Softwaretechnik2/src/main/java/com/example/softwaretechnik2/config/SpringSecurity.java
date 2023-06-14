a75525adaf705ca9f745c3ce94a8baafe5718e8b
BlendSalihu <blendsalihu@stud.th-luebeck.de>
2023-06-14 23:51:11 +0200

    #47 added comments in the SpringSecurity.class

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/config/SpringSecurity.java
+++ Softwaretechnik2/src/main/java/com/example/softwaretechnik2/config/SpringSecurity.java
@@ -16,38 +16,41 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SpringSecurity {
 
-
+    // Autowired UserDetailsService for authentication
     @Autowired
     private UserDetailsService userDetailsService;
 
+    // Bean for password encoder using BCrypt
     @Bean
-    public static PasswordEncoder passwordEncoder(){
+    public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // Bean for security filter chain configuration
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/register/**").permitAll()
-                .antMatchers("/shopinformation/edit").hasRole("EMPLOYEE")
-                .antMatchers("/produkterstellung").hasRole("EMPLOYEE")
-                .anyRequest().authenticated()
+                .antMatchers("/register/**").permitAll() // Allow access to registration without authentication
+                .antMatchers("/shopinformation/edit").hasRole("EMPLOYEE") // Restrict access to edit shop information to employees only
+                .antMatchers("/produkterstellung").hasRole("EMPLOYEE") // Restrict access to product creation to employees only
+                .anyRequest().authenticated() // Require authentication for any other requests
                 .and()
-                .formLogin(form -> form.loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/start", true)
-                        .failureUrl("/login-error")
-                        .permitAll())
-                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/login")
-                        .permitAll());
+                .formLogin(form -> form.loginPage("/login") // Set custom login page
+                        .loginProcessingUrl("/login") // Set login processing URL
+                        .defaultSuccessUrl("/start", true) // Redirect to /start after successful login
+                        .failureUrl("/login-error") // Redirect to /login-error after failed login
+                        .permitAll()) // Allow access to login page without authentication
+                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // Set logout request matcher
+                        .logoutSuccessUrl("/login") // Redirect to /login after successful logout
+                        .permitAll()); // Allow access to logout without authentication
         return http.build();
     }
 
+    // Global authentication configuration
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+                .userDetailsService(userDetailsService) // Set the custom UserDetailsService
+                .passwordEncoder(passwordEncoder()); // Set the password encoder
     }
 }
