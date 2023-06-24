a2da6f0596c377d712884b6e5aa027045aebd8c4
kawtharalshaaer <Kawthar.alshaaer@stud.th-luebeck.de>
2023-06-24 17:09:39 +0200

    #17 deleted unneeded classes

--- Softwaretechnik2/src/main/java/com/example/softwaretechnik2/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.softwaretechnik2.repositories;
-
-import com.example.softwaretechnik2.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
-}
